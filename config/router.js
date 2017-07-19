3d8d89bee12551c6048fa8c5b36e99b59c921b45
HCI01\rbradford <rbradford@hcpci.com>
2017-07-19 15:36:35 -0400

    Home Screen

--- config/router.js
+++ config/router.js
@@ -1,3 +1,30 @@
 import React from 'react';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+
+
+const Tabs = TabNavigator({
+  Home: {    
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => <Icon type='font-awesome' name="home" size={35} color={tintColor} />,
+    }, 
+  },
+  Search: {    
+    navigationOptions: {
+      tabBarLabel: 'Search',
+      tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
+    },
+  },
+});
+
+Tabs.navigationOptions = {
+  title: 'MY TITLE',
+  header: {
+    style: {
+      backgroundColor: '#42a5f5',
+    }
+  },
+};
+
+export default Tabs;
---
+++ screens/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  ScrollView
+} from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+
+class Home extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <List>          
+        </List>
+      </ScrollView>
+    );
+  }
+}
+
+export default Home;
